c154b5643efb001de4eaa5999b96799211780933
seciloztrk <secilozturk244@gmail.com>
2023-04-18 16:54:31 +0300

    homework1-patika

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import getData from "./callback";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getUserInfo() {
+  try {
+    // Kullanıcıdan id bilgisi istenir
+    const userId = Number(prompt("Enter user id:"));
+    // getData fonksiyonu çağrılır ve kullanıcının verileri alınır
+    const userData = await getData(userId);
+    console.log(userData); // kullanıcının verileri konsola yazdırılır
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-export default App;
+getUserInfo();
+
+
